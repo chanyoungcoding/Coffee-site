2f40feccc9b8da2baef423ef62b82f6c8ef4ac36
chan young <white1614@naver.com>
2023-09-15 23:52:40 +0900

    navbar 최종 수정 완료

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import '../styles/navbar.scss'
 import coffeeIcon from '../assets/coffeeicon.png'
 
 const Navbar:React.FC = () => {
+
+  const [visible, setVisible] = useState(false);
+
+  const toggleNavVisible = () => {
+    setVisible(!visible)
+  }
+
   return (
     <div className="navbar">
-      <div className="navbar__logo">
+      <div className="navbar__logo" onClick={toggleNavVisible}>
         <img src={coffeeIcon} alt="#" />
       </div>
       <div className="navbar__li">
-        <div className="li__left">
-          <li>HOME</li>
-          <li>MENU</li>
-          <li>RECIPE</li>
+        <div className="li__inner">
+          <div className={`li__left ${visible ? "visible" : ""}`}>
+            <li>HOME</li>
+            <li>MENU</li>
+            <li>RECIPE</li>
+          </div>
         </div>
-        <div className="li__right">
-          <li>MADE</li>
-          <li>INFROMATION</li>
+        <div className="li__inner">
+          <div className={`li__right ${visible ? "visible" : ""}`}>
+            <li>MADE</li>
+            <li>INFROMATION</li>
+          </div>
         </div>
       </div>
     </div>
