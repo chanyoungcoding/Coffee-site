a9fcf6dc0fb8aa5646d57a7f1cb00bd9a7850a6c
chan young <white1614@naver.com>
2023-09-16 15:34:30 +0900

    footer 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,31 @@
-const Footer = () => {
+import React from "react";
+import { AiFillFacebook, AiFillInstagram, AiFillYoutube} from "react-icons/ai";
+import coffeeIcon from '../assets/coffeeicon.png'
+import '../styles/footer.scss'
+
+const getCurrentYear = () => new Date().getFullYear();
+
+const Footer:React.FC = () => {
   return ( 
-    <div>
-      <p>Footer</p>
+    <div className="footer">
+      <div className="footer__inner">
+        <div className="inner__introduce">
+          <p>CHAN COFFEE - &#169; {getCurrentYear()}</p>
+          <p>hello</p>
+        </div>
+        <img src={coffeeIcon} alt="#" />
+        <div className="inner__icon">
+          <a href="javascript:void(0)">
+            <AiFillFacebook size ="40"/>
+          </a>
+          <a href="javascript:void(0)">
+            <AiFillInstagram size ="40"/>
+          </a>
+          <a href="javascript:void(0)">
+            <AiFillYoutube size ="40"/>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
