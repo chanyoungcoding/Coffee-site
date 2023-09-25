50621a87f7d9cb7d46396eb569d13e95ade14170
chan young <white1614@naver.com>
2023-09-25 16:03:46 +0900

    menu site 수정중

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Home from './pages/Home'
-import { Routes, Route } from 'react-router-dom'
 import Common from './pages/Common'
+import Menu from './components/Menu/Main'
+import { Routes, Route } from 'react-router-dom'
 import './styles/common.scss'
 
 import Test from './components/Test'
@@ -12,6 +13,7 @@ const App:React.FC = () => {
       <Route element={<Common/>}>
         <Route path="/" element={<Home/>}/>
         <Route path='/test' element={<Test/>}/>
+        <Route path='/menu' element={<Menu/>}/>
       </Route>
     </Routes>
   )
--- client/src/components/Menu/Main.tsx
+++ client/src/components/Menu/Main.tsx
@@ -1,9 +1,44 @@
-
+import coffeeCup3 from '../../assets/coffeecup3.png'
+import '../../styles/menu.scss'
 
 const Main = () => {
   return ( 
     <div className="menu">
-      <h1>에스프레소</h1>
+      <h1>아이스 아메리카노</h1>
+      <h2>Ice Americano</h2>
+      <div className="menu__inner">
+        <img src={coffeeCup3} alt="#" />
+        <div className="inner__menuintro">
+          <div className="menuintro__detail">
+            <h3>칼로리</h3>
+            <h4>5kcal</h4>
+          </div>
+          <div className="menuintro__detail__1">
+            <h3>1회 제공량</h3>
+            <h4>500ml</h4>
+          </div>
+        </div>
+        <div className="inner__menuintro">
+          <div className="menuintro__detail">
+            <h3>나트륨</h3>
+            <h4>5kcal</h4>
+          </div>
+          <div className="menuintro__detail__1">
+            <h3>단백질</h3>
+            <h4>500ml</h4>
+          </div>
+        </div>
+        <div className="inner__menuintro">
+          <div className="menuintro__detail">
+            <h3>당유</h3>
+            <h4>5kcal</h4>
+          </div>
+          <div className="menuintro__detail__1">
+            <h3>카페인</h3>
+            <h4>500ml</h4>
+          </div>
+        </div>
+      </div>
       
     </div>
   );
