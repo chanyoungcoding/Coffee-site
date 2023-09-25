74c052344a9eda86c8849648d9b9a814f679f44a
chan young <white1614@naver.com>
2023-09-25 16:19:02 +0900

    menu page 수정 완료

--- client/src/components/Menu/Main.tsx
+++ client/src/components/Menu/Main.tsx
@@ -11,31 +11,31 @@ const Main = () => {
         <div className="inner__menuintro">
           <div className="menuintro__detail">
             <h3>칼로리</h3>
-            <h4>5kcal</h4>
+            <h4>10Kcal</h4>
           </div>
           <div className="menuintro__detail__1">
-            <h3>1회 제공량</h3>
-            <h4>500ml</h4>
+            <h3>1잔</h3>
+            <h4>1잔-355ml</h4>
           </div>
         </div>
         <div className="inner__menuintro">
           <div className="menuintro__detail">
             <h3>나트륨</h3>
-            <h4>5kcal</h4>
+            <h4>5mg</h4>
           </div>
           <div className="menuintro__detail__1">
             <h3>단백질</h3>
-            <h4>500ml</h4>
+            <h4>1g</h4>
           </div>
         </div>
         <div className="inner__menuintro">
           <div className="menuintro__detail">
             <h3>당유</h3>
-            <h4>5kcal</h4>
+            <h4>0g</h4>
           </div>
           <div className="menuintro__detail__1">
             <h3>카페인</h3>
-            <h4>500ml</h4>
+            <h4>150ml</h4>
           </div>
         </div>
       </div>
