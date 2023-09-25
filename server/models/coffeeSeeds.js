83f2faad39eba945ffe32eddd76c51b7ae81d063
chan young <white1614@naver.com>
2023-09-25 18:01:49 +0900

    서버 수정 완료

--- server/models/coffeeSeeds.js
+++ server/models/coffeeSeeds.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const coffeeDetail = require('./커피목록.json');
 
 const dbUrl = "mongodb://127.0.0.1:27017/coffeeDB"
 
@@ -22,17 +23,20 @@ const CoffeeSchema = new Schema({
 
 const Coffee = mongoose.model('Coffee', CoffeeSchema);
 
+
 const coffeeDB = async () => {
   await Coffee.deleteMany({});
   for(let i = 0; i < 10; i++) {
+    const Espresso = coffeeDetail.Espresso[i];
+
     const coffee = new Coffee({
-      coffeeName: '아메리카노',
-      calory:5,
-      sodium: 5,
-      protein: 5,
-      sugar: 5,
-      caffeine: 5,
-      price: 2500,
+      coffeeName: Espresso.name,
+      calory: Espresso.calory,
+      sodium: Espresso.sodium,
+      protein: Espresso.protein,
+      sugar: Espresso.sugar,
+      caffeine: Espresso.caffeine,
+      sat_fat:Espresso.sat_fat,
       description: '아메리카노는 에스프레소의 진한 풍미를 잘 느낄 수 있는 음료입니다.'
     })
     await coffee.save();
