f2cd27cc0e69c782af70210dc1234e12838c843b
chan young <white1614@naver.com>
2023-11-01 13:26:28 +0900

    좋아요 컴포넌트 분리

---
+++ client/src/components/Recipe/RecipeDetail/RecipeGreat.tsx
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import Cookies from "js-cookie";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+
+import { RecipeData } from "../../../models/coffee";
+import { useBasketMutation } from "../../../services/api";
+
+const RecipeGreat:React.FC<RecipeData> = ({data}) => {
+
+  const [disabled, setDisabled] = useState(false);
+  const { mutate } = useBasketMutation();
+
+  const onClick = (e:React.MouseEvent<HTMLButtonElement>, coffeeName:string, coffeeUrl:string) => {
+    const userName = Cookies.get('사용자명')
+    setDisabled(disabled => !disabled)
+    e.currentTarget.disabled = true;
+    mutate({coffeeName, coffeeUrl ,userName});
+  }
+
+  return ( 
+    <>
+      {data?.map(item => (
+        <button 
+          key={item._id} 
+          className="good__button"
+          onClick={(e) => onClick(e, item.name, item.imgurl)}
+        >
+          {disabled ? <AiFillHeart size="40"/> : <AiOutlineHeart size="40"/>}
+        </button>
+      ))}
+    </>
+  );
+}
+export default RecipeGreat;
