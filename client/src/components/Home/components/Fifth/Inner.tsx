b4f6cfa577008719f27f357583d310fb115fa294
chan young <white1614@naver.com>
2023-11-14 19:18:45 +0900

    styled components 적용 및 Home 반응형 완성

--- client/src/components/Home/components/Fifth/Inner.tsx
+++ client/src/components/Home/components/Fifth/Inner.tsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Button from "../../../Button";
+import styled from "styled-components";
+
+const InnerBox = styled.div`
+  h1 {
+    font-size: 3rem;
+  }
+  h2 {
+    margin: 10px 0px 30px;
+    font-size: 2rem;
+    @media (max-width: 500px) {
+      font-size: 20px;
+    }
+  }
+`
 
 const Inner:React.FC = () => {
   return ( 
-    <div className="inner__box">          
+    <InnerBox className="inner__box">          
       <h1>레시피</h1>
       <h2>커피 레시피를 참고해서 커피를 만들어 보세요.</h2>
       <Button href="/recipeHome">보러 가기</Button>
-    </div>
+    </InnerBox>
   );
 }
 
--- client/src/components/Home/components/Fifth/Line.tsx
+++ client/src/components/Home/components/Fifth/Line.tsx
@@ -1,12 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+
+const LineContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 1920px;
+  position: absolute;
+  top: -40px;
+  height: 100px;
+  background-color: #361306;
+  transform: rotate(-2deg);
+  p {
+    color: rgb(233, 233, 233);
+    font-size: 3rem;
+  }
+`
 
 const Line:React.FC = () => {
   const lineText = ['COFFEE','COFFEE','COFFEE','COFFEE','COFFEE','COFFEE'];
 
   return ( 
-    <div className="homefifth__line">
+    <LineContainer className="homefifth__line">
       {lineText.map((text,index) => (<p key={index}>{text}</p>))}
-    </div>
+    </LineContainer>
   );
 }
 export default Line;
